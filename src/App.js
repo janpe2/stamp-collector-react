3bc69c8e6371be44e83a0e9e3ff71fef3a22e46c
Jani Pehkonen <janpe2@users.noreply.github.com>
2020-05-19 12:54:04 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container">
+        <h1>Otsikko</h1>
+
+      </div>
+    </div>
+  );
+}
+
+export default App;
