ccbfbe0b0fff9b8cdd505f4ca8c898ab52dde523
Jani Pehkonen <janpe2@users.noreply.github.com>
2020-05-28 09:44:29 +0300

    Updates

---
+++ precache-manifest.c18ced95608880fead01cee6ed28ba78.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9c9f99c2ab672e6b4bae3102ec3867e",
+    "url": "/stamp-collector-react/index.html"
+  },
+  {
+    "revision": "f63b914bb4d37485f843",
+    "url": "/stamp-collector-react/static/css/main.702e7e3c.chunk.css"
+  },
+  {
+    "revision": "c9ccd7735fa8c33c3955",
+    "url": "/stamp-collector-react/static/js/2.18a67e47.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/stamp-collector-react/static/js/2.18a67e47.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f63b914bb4d37485f843",
+    "url": "/stamp-collector-react/static/js/main.c83ae179.chunk.js"
+  },
+  {
+    "revision": "8fb4cf4b9c3b80e86bb5",
+    "url": "/stamp-collector-react/static/js/runtime-main.0e800b07.js"
+  }
+]);
